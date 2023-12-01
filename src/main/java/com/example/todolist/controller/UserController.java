bb1203ffbd7657255fb79cc203ce29232f1df8a5
m124578n <m23568n@gmail.com>
2023-12-01 09:43:51 +0800

    add user login/update info/logout controller

--- src/main/java/com/example/todolist/controller/UserController.java
+++ src/main/java/com/example/todolist/controller/UserController.java
@@ -2,16 +2,18 @@ package com.example.todolist.controller;
 
 import com.example.todolist.controller.dto.Response;
 import com.example.todolist.controller.dto.UserDto;
+import com.example.todolist.controller.dto.UserLoginDto;
+import com.example.todolist.controller.dto.UserUpdateDto;
 import com.example.todolist.model.User;
 import com.example.todolist.service.UserService;
 import com.example.todolist.util.Hash;
+import io.jsonwebtoken.lang.Strings;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +25,7 @@ public class UserController {
     Map<String, String> message = new HashMap<>();
 
     @Autowired
-    UserService userService;
+    UserService userSvc;
 
     @PostMapping("/register")
     public Response UserRegister(@Valid @RequestBody UserDto data, BindingResult bindingResult) {
@@ -39,11 +41,128 @@ public class UserController {
             String pwdHash = Hash.generateHash(data.account, data.password);
             user.setPwdHash(pwdHash);
 
-            userService.UserRegister(user);
+            userSvc.UserRegister(user);
         } catch (Exception e) {
             return new Response().Error().ErrorMessage(e);
         }
 
         return new Response();
+    }
+
+    /**
+     * 使用者登入
+     */
+    @PostMapping("/login")
+    public Response UserLogin(@Valid @RequestBody UserLoginDto data, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return new Response().Error().ErrorMessage(bindingResult.getAllErrors());
+        }
+
+        try {
+
+            // 使用者登入
+            User user = userSvc.UserLogin(data.account, data.password);
+
+            // 產生 Token
+            String token = userSvc.GenerateToken(user);
+
+            Map<String, String> res = new HashMap<>();
+            res.put("token", token);
+
+            return new Response().AddData(res);
+        } catch (Exception e) {
+            return new Response().Error().ErrorMessage(e);
+        }
+
+    }
+
+    /**
+     * 取得使用者資訊
+     */
+    @GetMapping("/info")
+    public Response UserInfo() {
+
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            Long userId = (Long) auth.getPrincipal();
+
+            User user = userSvc.GetUserInfoById(userId);
+
+            return new Response().AddData(user);
+        } catch (Exception e) {
+
+            return new Response().Error().ErrorMessage(e);
+
+        }
+
+    }
+
+    /**
+     * 更新使用者名稱
+     */
+    @PutMapping("/info/name")
+    public Response UpdateUserName(@RequestBody UserUpdateDto req) {
+        // 更新name則name不可為空
+        if (!Strings.hasText(req.name)) {
+            return new Response().Error().ErrorMessage(new IllegalArgumentException("名稱不可為空"));
+        }
+
+        try {
+
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            Long userId = (Long) auth.getPrincipal();
+
+            userSvc.UpdateUserName(userId, req.name);
+
+            return new Response();
+        } catch (Exception e) {
+            return new Response().Error().ErrorMessage(e);
+
+        }
+
+    }
+
+    /**
+     * 更新使用者名稱
+     */
+    @PutMapping("/info/password")
+    public Response UpdateUserPassword(@RequestBody UserUpdateDto req) {
+        // 更新password則password不可為空
+        if (!Strings.hasText(req.password)) {
+            return new Response().Error().ErrorMessage(new IllegalArgumentException("新密碼不可為空"));
+        }
+
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            Long userId = (Long) auth.getPrincipal();
+
+            userSvc.UpdateUserPassword(userId, req.password);
+
+            return new Response();
+        } catch (Exception e) {
+            return new Response().Error().ErrorMessage(e);
+
+        }
+
+    }
+
+    /**
+     * 登出
+     */
+    @PostMapping("/logout")
+    public Response Logout() {
+
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            Long userId = (Long) auth.getPrincipal();
+
+            userSvc.Logout(userId);
+            return new Response();
+        } catch (Exception e) {
+            return new Response().Error().ErrorMessage(e);
+
+        }
+
     }
 }
--- src/main/java/com/example/todolist/controller/dto/Response.java
+++ src/main/java/com/example/todolist/controller/dto/Response.java
@@ -3,10 +3,13 @@ package com.example.todolist.controller.dto;
 import org.springframework.validation.ObjectError;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Response {
     public String message;
+    public Object data = new HashMap<>();
     public ArrayList<String> error;
 
     public Response() {
@@ -26,6 +29,16 @@ public class Response {
 
     public Response ErrorMessage(Throwable e) {
         this.error.add(e.getMessage());
+        return this;
+    }
+
+    public Response AddData(Map<String, String> data) {
+        this.data = data;
+        return this;
+    }
+
+    public Response AddData(Object obj) {
+        this.data = obj;
         return this;
     }
 }
